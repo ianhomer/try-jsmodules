e6c7dcc16f57d62432c6c106819121eda6c4951a
Ian Homer <ian.homer@gmail.com>
2022-02-16 21:07:01 +0000

    feat: use string templates

--- packages/commonjs/index.js
+++ packages/commonjs/index.js
@@ -1,4 +1,4 @@
 var foo = require("./foo.js");
 
 console.log("Try CommonJs");
-console.log("2 times 2 : " + foo.timesTwo(2));
+console.log(`2 times 2 : ${foo.timesTwo(2)}`);
--- packages/esm/index.js
+++ packages/esm/index.js
@@ -2,4 +2,4 @@ import { timesTwo } from "./foo.js";
 
 console.log("Try ESM");
 
-console.log("2 times 2 : " + timesTwo(2));
+console.log(`2 times 2 : ${timesTwo(2)}`);
